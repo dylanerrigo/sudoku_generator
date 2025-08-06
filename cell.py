9f92d1f88937dadd42a3c2d1afa196f62c417f69
AntonioNohra <antonionohra2004@gmail.com>
2025-08-06 15:32:52 -0400

    Fixed color cell.py

--- cell.py
+++ cell.py
@@ -1,23 +1,28 @@
 import pygame
 
 class Cell:
-  def __init__(self, value, row, col, screen):
-    self.value = value
-    self.sketched_value = 0
-    self.row = row
-    self.col = col
-    self.screen = screen
-    self.selected = False
-  def set_cell_value(self, value):
-    self.value = value
-  def set_sketched_value(self, value):
-    self.sketched_value = value
+    def __init__(self, value, row, col, screen, is_user_input=False):
+        self.value = value
+        self.sketched_value = 0
+        self.row = row
+        self.col = col
+        self.screen = screen
+        self.selected = False
+        self.is_user_input = is_user_input
 
-  def clear(self):
-      self.value = 0
-      self.sketched_value = 0
+    def set_cell_value(self, value):
+        self.value = value
+        self.is_user_input = True
 
-  def draw(self, width, height):
+    def set_sketched_value(self, value):
+        self.sketched_value = value
+
+    def clear(self):
+        self.value = 0
+        self.sketched_value = 0
+        self.is_user_input = False
+
+    def draw(self, width, height):
         x_pos = self.col * width
         y_pos = self.row * height
 
@@ -27,10 +32,12 @@ class Cell:
         font = pygame.font.Font(None, 40)
 
         if self.value != 0:
-            text = font.render(str(self.value), True, (0, 0, 0))
+            color = (0, 0, 255) if self.is_user_input else (0, 0, 0)
+            text = font.render(str(self.value), True, color)
             text_rect = text.get_rect(center=(x_pos + width / 2, y_pos + height / 2))
             self.screen.blit(text, text_rect)
         elif self.sketched_value != 0:
             text = font.render(str(self.sketched_value), True, (0, 0, 255))
             text_rect = text.get_rect(center=(x_pos + width / 2, y_pos + height / 2))
             self.screen.blit(text, text_rect)
+
